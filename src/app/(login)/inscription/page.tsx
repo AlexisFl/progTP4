6ac6fc7681fb8272fb2220b4b5567a76349b201a
af303746 <alexis_falconnet@etu.u-bourgogne.fr>
2023-11-24 10:25:11 +0100

    Question 2.2

--- src/app/(login)/inscription/page.tsx
+++ src/app/(login)/inscription/page.tsx
@@ -4,7 +4,8 @@ import React from 'react';
 import { useForm, zodResolver } from '@mantine/form';
 import { z } from 'zod';
 import { TextInput, PasswordInput, Box } from '@mantine/core';
-import { Button } from 'tp-kit/components';
+import {Button, NoticeMessage, NoticeMessageData} from 'tp-kit/components';
+import {useState} from "react";
 
 const schema = z.object({
     name: z.string().nonempty({ message: 'Le nom est requis' }),
@@ -22,11 +23,39 @@ const SignupForm = () => {
         },
     });
 
+    const [notices, setNotices] = useState<NoticeMessageData[]>([]);
+
+    function addError() {
+        setNotices( n => [...n, {type: "error", message: "Cette adresse n'est pas disponible"}])
+    }
+
+    function addSuccess() {
+        setNotices( n => [...n, {type: "success", message: "Votre Inscription a bien été prise en compte. Validez votre adresse email pour vous connecter."}])
+    }
+
+    function removeNotice (index: number){
+        setNotices(n => {
+            delete(n[index]);
+            return Object.values(n)
+        });
+    }
+
+    const handleSignup = () => {
+        addError();
+        addSuccess();
+    };
 
 
     return (
         <Box maw={340} mx="auto">
             <form onSubmit={form.onSubmit((values) => console.log(values))} className="space-y-8 mt-16">
+                <ul>
+                    {notices.map((notice, i) => <NoticeMessage
+                        key={i}
+                        {...notice}
+                        onDismiss={() => removeNotice(i)}
+                    />)}
+                </ul>
                 <TextInput
                     withAsterisk
                     label="Nom"
@@ -47,7 +76,7 @@ const SignupForm = () => {
                     {...form.getInputProps('password')}
                 />
                 <div>
-                    <Button type="submit" fullWidth>
+                    <Button type="submit" onClick={handleSignup} fullWidth>
                         S'inscrire
                     </Button>
                     <Button type="button" fullWidth variant="ghost">
